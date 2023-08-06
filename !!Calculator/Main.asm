0880493bb6086aeb1c7a1ecd662f55d0b704dc6a
IntelSDM <GitBackup@shibemaster.me>
2023-08-06 12:06:53 +0100

    Subtraction With Error Handling

--- !!Calculator/Main.asm
+++ !!Calculator/Main.asm
@@ -7,6 +7,7 @@ OperatorMessage BYTE "Select Operator(+,-,*,/):",0
 FirstInput BYTE "Input First Number: ",0
 SecondInput BYTE "Input Second Number: ",0
 ResultMessage BYTE "Result:",0
+SubtractionErrorMessage BYTE "First Number Is Greater Than Second Number. Retry with a correct number combination.",0
 .code
 main PROC
     mov eax, 9h ; Blue text
@@ -19,6 +20,7 @@ main PROC
     call Crlf ; New line
     call Crlf ; New line
 
+    Operator:
     ; Operator Input
     mov edx, OFFSET OperatorMessage
     call WriteString
@@ -27,13 +29,15 @@ main PROC
 
 
     call Crlf  ; New line
-
+    
+    Input1:
     ;First Number Input
     mov edx, OFFSET FirstInput
     call WriteString
     call ReadInt            ; Convert to int
     mov ecx, eax            ; Num 1 in ecx
 
+    Input2:
     ; Second Number Input
     mov edx, OFFSET SecondInput
     call WriteString
@@ -44,11 +48,27 @@ main PROC
     ; Just compare text to our operators, otherwise kill program
     cmp bl, '+'
     je AddNumbers
+    cmp bl, '-'
+    je SubtractNumbers
     jmp ExitProgram 
 
 AddNumbers:
-    call Crlf ; new line
     add ecx, edx
+    jmp Result
+
+SubtractNumbers:
+    sub ecx, edx
+    js NegativeError   ; Jump to negative error if result is negative
+    jmp Result
+
+NegativeError:
+    mov edx, OFFSET SubtractionErrorMessage
+    call WriteString
+    call Crlf ; New line
+    jmp Input1
+
+Result:
+    call Crlf ; New line
     mov edx, OFFSET ResultMessage
     call WriteString
     mov eax, ecx            ; ECX to EAX for writeint
